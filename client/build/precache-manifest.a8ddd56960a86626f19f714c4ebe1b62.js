2eb919128c200cd87d1c2ce9eff03de5bd0b122e
itshuey <hubert.s.sun@gmail.com>
2020-04-09 12:23:26 -0700

    Adding chunks

---
+++ client/build/precache-manifest.a8ddd56960a86626f19f714c4ebe1b62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84afda515777df75771e955a7ed6216a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18ff3b7ff644ea82ac6e",
+    "url": "/static/css/main.0bdb3f49.chunk.css"
+  },
+  {
+    "revision": "928ee8e6bafe3ee54161",
+    "url": "/static/js/2.44e0a307.chunk.js"
+  },
+  {
+    "revision": "a360305518243deee3bfebd4256d9edd",
+    "url": "/static/js/2.44e0a307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ff3b7ff644ea82ac6e",
+    "url": "/static/js/main.a8d5e70f.chunk.js"
+  },
+  {
+    "revision": "a964a5a02a76936dc6f7",
+    "url": "/static/js/runtime-main.1d16e1e0.js"
+  },
+  {
+    "revision": "811fbfd1e7865a349a0ebd9d1104987c",
+    "url": "/static/media/q.811fbfd1.png"
+  }
+]);
