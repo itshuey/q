03b16efa9466ff500faf175380a74e428647489c
itshuey <hubert.s.sun@gmail.com>
2020-04-09 11:51:58 -0700

    Cleaning up code, bumping version

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const qitems = require('./routes/api/qitems');
 const cors = require('cors');
 const path = require('path');
-const qitems = require('./routes/api/qitems');
 const app = express();
 
+// Connect to MongoDB backend
 connectDB();
 app.use(cors({ origin: true, credentials: true }));
-
 app.use(express.json({ extended: false }));
+
+// Include routes & front-end
 app.use(express.static(__dirname + '/client/build'));
-// app.use(express.static('/client/build'));
 app.use('/api/qitems', qitems);
 
-// Serve front-end
-// app.get('/', (req, res) => res.send('Hello world!'));
+// Serve front-end code
 app.get('/', (req, res) => {
-  // res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
   res.sendFile('client/build/index.html', { root: __dirname });
 });
 
-const port = process.env.PORT || 8082;
-// app.listen(port, () => console.log(`Server running on port ${port}`));
-
-// Attach socket to server
+// Attach socket to server (port share)
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
+// Start server
+const port = process.env.PORT || 8082;
 server.listen(port, () => console.log(`Server running on port ${port}`));
 
+// Configure socket
 io.on('connection', function (socket) {
   socket.emit('connect', { hello: 'world' });
 
